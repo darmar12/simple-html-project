d6c9cbefd624e062c554d25f3a82add01a5168c9
Dima Ovharenko <dima-o@mail.ru>
2024-02-28 22:50:41 +0300

    add slider block

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+    $('.slider_container').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+    });
+});
